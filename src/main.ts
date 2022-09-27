8701529ad1aca645e283b99feedd317376783416
Ahmed Radwan <aradwann@proton.me>
2022-09-28 01:53:51 +0200

    configure validation, swagger, cors for the app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //////////////////// SETUP VALIDATION PIPE ///////////////
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // setting whitelist to true trim any additional unwanted props/values in the request body
+      // that's not defined by the dto and doesn't have validation decorators
+      whitelist: true,
+      transformOptions: {
+        // enable implicit conversion of the received params over the network
+        // as the network transfer all values as strings we need to convert them
+        // to their correct types to be appropriatly used
+        // such as query params used in pagination
+        // enabling this option makes the params converted to integers automatically
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  ////////////////////////////// SETUP SWAGGER (OPEN API) ////////////////////////
+  const options = new DocumentBuilder()
+    .setTitle('uptime')
+    .setDescription(
+      'uptime is a utility to monitor and log the availability \
+      of the URLs added by authenticated users and get the report for its uptime and downtime ',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  ////////////////////////// CORS ///////////////////////////////////
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
